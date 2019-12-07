7eca1a56c3ac7c0e0e6c5d43536f9f65bc40dffb
emmamack <42980990+emmamack@users.noreply.github.com>
2019-12-07 15:57:25 -0500

    experiment with integration

--- dtf_accel_data.m
+++ dtf_accel_data.m
@@ -1,12 +1,15 @@
 t = linspace(1,829,829);
-N = length(az_flower);
+N = length(az);
 Fs = 85;
 f_double = linspace(-Fs/2, Fs/2 - Fs/N, N) + Fs/(2*N)*mod(N, 2);
-half_length = floor(length(frequencies_shifted)/2);
+half_length = floor(length(f_double)/2);
 f = f_double(length(f_double)-half_length + 1:end);
-AZ_double = fft(az_flower);
+AZ_double = fft(az);
 AZ = AZ_double(1:half_length);
-plot(f,abs(AZ))
+%plot(f,abs(AZ))
+plot(f_double,abs(fftshift(AZ_double)))
+xlabel("Frequency (Hz)")
+ylabel("Magnitude")
 
 [sorted_AZ,ind] = sort(AZ, 'descend');
 max_ind = ind(2);
